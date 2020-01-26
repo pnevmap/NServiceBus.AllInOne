cb3e420a8cf0db0c27564c04a316b7a182172195
Panos Pnevmatikatos <ppnevmatikatos@oneviewhealthcare.com>
2020-01-26 22:22:52 +0000

    add qa functionality

---
+++ src/Shared.Hosting.Abstractions.Tests/Logging/QaExtensions.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+namespace Shared.Hosting.Abstractions.Tests.Logging
+{
+    public static class QaExtensions
+    {
+        public static string[] ToConfigurationArgs(this ICollection<KeyValuePair<string, string>> dic)
+        {
+            var result = new string[dic.Count];
+
+            var i = 0;
+
+            foreach (var pair in dic)
+                result[i++] = $"{pair.Key}={pair.Value}";
+
+            return result;
+        }
+        public static string[] ToConfigurationArgs(this object input)
+        {
+            return JsonConfigurationParser.Parse(input).ToConfigurationArgs();
+        }
+    }
+}
