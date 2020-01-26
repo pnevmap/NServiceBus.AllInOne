60b78b9b0b489d6dc8fd194f8e235532c3e7076f
Panos Pnevmatikatos <ppnevmatikatos@oneviewhealthcare.com>
2020-01-26 22:21:51 +0000

    simplify LoggingPolicy class

--- src/Shared.Hosting.Abstractions/Logging/LoggingPolicy.cs
+++ src/Shared.Hosting.Abstractions/Logging/LoggingPolicy.cs
@@ -9,17 +9,17 @@ namespace Shared.Hosting.Abstractions.Logging
     public class LoggingPolicy : ILoggingPolicy
     {
         public string Section { get; set; }
-    
+
         public string OutputTemplate { get; set; }
         public MinimumLevelOptions MinimumLevel { get; } = new MinimumLevelOptions();
-        
+
         public class MinimumLevelOptions
         {
             public LogEventLevel Default { get; set; } = LogEventLevel.Verbose;
 
             public Dictionary<string, LogEventLevel> Override { get; } = new Dictionary<string, LogEventLevel>();
         }
-        
+
         public string Path { get; set; } = "./logs/{{{ApplicationName}}}-{Hour}.log";
 
         public bool IsDefined()
@@ -32,46 +32,24 @@ namespace Shared.Hosting.Abstractions.Logging
         {
             if (!string.IsNullOrWhiteSpace(Section))
             {
-                logConfig
-                    .ReadFrom.Configuration(configuration, Section);
-                return;                
+                logConfig.ReadFrom.Configuration(configuration, Section);
+                return;
             }
-            
+
             var path = Path.Replace("{{{ApplicationName}}}", applicationName);
-                
+
             logConfig
                 .MinimumLevel.Is(MinimumLevel.Default)
                 .WriteTo.Async(a =>
                 {
-                    if (IsJsonOutput())
-                    {
-                        a.Console(outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level}] <{SourceContext}> {Message}{NewLine}{Exception}");
-                        a.RollingFile(pathFormat: path, shared: true, formatter: new Serilog.Formatting.Compact.RenderedCompactJsonFormatter());
-                    }
-                    else
-                    {
-                        a.Console(outputTemplate: OutputTemplate);
-                        a.RollingFile(pathFormat: path, shared: true, outputTemplate: OutputTemplate);
-                    }
+                    a.Console(outputTemplate: OutputTemplate);
+                    a.RollingFile(pathFormat: path, shared: true, outputTemplate: OutputTemplate);
                 });
 
-            if (MinimumLevel.Override == null || !MinimumLevel.Override.Any())
+            foreach (var level in MinimumLevel.Override)
             {
-                logConfig.MinimumLevel.Override("Microsoft", LogEventLevel.Information);
-                logConfig.MinimumLevel.Override("Shared.Web.Middleware.Logging", LogEventLevel.Warning);
+                logConfig.MinimumLevel.Override(level.Key, level.Value);
             }
-            else
-            {
-                foreach (var level in MinimumLevel.Override)
-                {
-                    logConfig.MinimumLevel.Override(level.Key, level.Value);
-                }
-            }
-        }
-
-        private bool IsJsonOutput()
-        {
-            return string.IsNullOrWhiteSpace(OutputTemplate) || string.Equals(OutputTemplate, "json");
         }
     }
 }
